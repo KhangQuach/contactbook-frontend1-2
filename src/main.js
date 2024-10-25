2239b35a7e65095bacb34b777bb1f3a3993da300
Khang Quách <khangB2203505@student.ctu.edu.vn>
2024-10-25 11:35:49 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import './assets/main.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import router from './router'
+library.add(faUserSecret)
+
+import { dom } from '@fortawesome/fontawesome-svg-core'
+dom.watch()
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(router)
+app.mount('#app')
