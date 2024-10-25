43d54ca7a5ac30edec470dfd0a4f815a81846749
Khang Quách <khangB2203505@student.ctu.edu.vn>
2024-10-25 11:52:41 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import { createWebHistory, createRouter } from "vue-router"
 const routes = [
-{
-  path: "/",
-  name: "contactbook",
-},
+  {
+    path: "/",
+    name: "contactbook",
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
